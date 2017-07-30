c9846613a5adaac7ab05ece6ed0f373361ec6dc4
SherMary <30530751+SherMary@users.noreply.github.com>
2017-07-30 15:02:22 +0300

    Добавлен тест создания контакта

---
+++ contact.py
@@ -0,0 +1,22 @@
+class Contact:
+    def __init__(self, firstname, middlename, lastname, nickname, title, company, address, telhome, telmobile, telwork, fax, email, email2, email3, homepage, birth, anniversary, address2, home2, notes):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nickname = nickname
+        self.title = title
+        self.company = company
+        self.address = address
+        self.telhome = telhome
+        self.telmobile = telmobile
+        self.telwork = telwork
+        self.fax = fax
+        self.email = email
+        self.email2 = email2
+        self.email3 = email3
+        self.homepage = homepage
+        self.birth = birth
+        self.anniversaryn = anniversary
+        self.address2 = address2
+        self.home2 = home2
+        self.notes = notes
