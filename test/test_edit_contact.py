c2da244df0bec83421096d84895c008ebc9e5b8d
SherMary <30530751+SherMary@users.noreply.github.com>
2017-08-04 17:10:15 +0300

    Добавлен тест редактирования группы

---
+++ test/test_edit_contact.py
@@ -0,0 +1,25 @@
+from model.contact import Contact
+
+def test_edit_contact(app):
+    app.session.login(username="admin", password="secret")
+    app.contact.edit_contact(Contact(firstname="Master",
+                               middlename="Missi",
+                               lastname="Cruel Person",
+                               nickname="Selfish lady",
+                               title="MASTER",
+                               company="Lords of time",
+                               address="Galifray?",
+                               telhome="123-56-2",
+                               telmobile="+7000854930",
+                               telwork="+89 090 9430288",
+                               fax="409238275902",
+                               email="master86@gal.com",
+                               email2="missiforever@tardis.net",
+                               email3="wheredoctoris@clever.com",
+                               homepage="mastermissi.com",
+                               birth="1000",
+                               anniversary="1500",
+                               address2="Galifray dimension",
+                               home2="Where doctor is",
+                               notes="Somewhere far beyond!"))
+    app.session.logout()
