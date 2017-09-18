548e22931b86fa145b5e841f0150891d139831c5
SherMary <30530751+SherMary@users.noreply.github.com>
2017-09-18 18:47:29 +0300

    Добавлен тест для проверки всех полей контакта на главной странице

--- test/test_data_on_home_page.py
+++ test/test_data_on_home_page.py
@@ -1,26 +1,16 @@
 import re
 
 
-def test_phones_on_home_page(app):
+def test_data_on_home_page(app):
     contact_from_home_page = app.contact.get_contact_list()[0]
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
     assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(
            contact_from_edit_page)
-
-
-def test_emails_on_home_page(app):
-    email_from_home_page = app.contact.get_contact_list()[0]
-    email_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
-    assert email_from_home_page.all_emails_from_home_page == merge_emails_like_on_home_page(
-        email_from_edit_page)
-
-
-def test_name_and_address_on_home_page(app):
-    name_and_address_from_home_page = app.contact.get_contact_list()[0]
-    name_and_address_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
-    assert name_and_address_from_home_page.firstname == name_and_address_from_edit_page.firstname
-    assert name_and_address_from_home_page.lastname == name_and_address_from_edit_page.lastname
-    assert name_and_address_from_home_page.address == name_and_address_from_edit_page.address
+    assert contact_from_home_page.all_emails_from_home_page == merge_emails_like_on_home_page(
+        contact_from_edit_page)
+    assert contact_from_home_page.firstname == contact_from_edit_page.firstname
+    assert contact_from_home_page.lastname == contact_from_edit_page.lastname
+    assert contact_from_home_page.address == contact_from_edit_page.address
 
 
 def clear(s):
