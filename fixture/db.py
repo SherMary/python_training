9388aa010b6c27a6acb909ef43066c03737e2345
SherMary <30530751+SherMary@users.noreply.github.com>
2017-09-28 11:12:21 +0300

    7й урок. Попытка забрать из базы список контактов, входящих в группу
    TypeError: no default type converter defined

--- fixture/db.py
+++ fixture/db.py
@@ -1,5 +1,7 @@
 import mysql.connector
 from model.group import Group
+from model.contact import Contact
+
 
 class DbFixture:
 
@@ -10,18 +12,32 @@ class DbFixture:
         self.user = user
         self.password = password
         self.connection = mysql.connector.connect(host=host, database=name, user=user, password=password)
+        self.connection.autocommit = True
 
     def get_group_list(self):
         list = []
         cursor = self.connection.cursor()
         try:
             cursor.execute("select group_id, group_name, group_header, group_footer from group_list")
-            for row in cursor:
+            for row in cursor.fetchall():
                 (id, name, header, footer) = row
                 list.append(Group(id=str(id), name=name, header=header, footer=footer))
         finally:
             cursor.close()
         return list
+
+    def get_contact_list(self):
+        list = []
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute("select id, firstname, lastname from addressbook where deprecated='0000-00-00 00:00:00'")
+            for row in cursor.fetchall():
+                (id, firstname, lastname) = row
+                list.append(Contact(id=str(id), firstname=firstname, lastname=lastname))
+        finally:
+            cursor.close()
+        return list
+
 
     def destroy(self):
         self.connection.close()
